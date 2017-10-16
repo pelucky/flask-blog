0361cda65fa7924ca23a028b062a63fdad56bb05
pelucky <zx8522588@gmail.com>
2017-10-16 21:37:09 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+setup(
+    name='Flask-blog',
+    version='1.0',
+    long_description=__doc__,
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'alembic==0.9.5',
+        'click==6.7',
+        'dominate==2.3.1',
+        'Flask==0.12.2',
+        'Flask-Bootstrap==3.3.7.1',
+        'Flask-Login==0.4.0',
+        'Flask-Markdown==0.3',
+        'Flask-Migrate==2.1.1',
+        'Flask-Script==2.0.6',
+        'Flask-SQLAlchemy==2.3.1',
+        'Flask-WTF==0.14.2',
+        'itsdangerous==0.24',
+        'Jinja2==2.9.6',
+        'Mako==1.0.7',
+        'Markdown==2.6.9',
+        'MarkupSafe==1.0',
+        'python-dateutil==2.6.1',
+        'python-editor==1.0.3',
+        'six==1.11.0',
+        'SQLAlchemy==1.1.14',
+        'visitor==0.1.3',
+        'Werkzeug==0.12.2',
+        'WTForms==2.1',
+    ]
+)
